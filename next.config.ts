9015350b7e8f5213df91c73b33672a2234bc4db3
boziz <bozize@gmail.com>
2025-03-22 16:29:16 +0300

    Initial commit

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  distDir: process.env.NODE_ENV === "production" ? "build" : ".next",
   images: {
     domains: [
       "source.unsplash.com",
@@ -33,3 +31,4 @@ const nextConfig: NextConfig = {
 };
 
 export default nextConfig;
+
